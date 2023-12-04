f7cea79e91d6761855bb9dfad24cfb2c01e8e5a3
mcrachel7 <mcrachel79@unitec.edu>
2023-12-04 02:13:18 -0600

    final commit

---
+++ src/clase1.java
@@ -0,0 +1,79 @@
+
+import java.util.logging.Logger;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Rachel
+ */
+public class clase1 {
+
+    
+    int equipo;
+    String volante; 
+    String referi; 
+    int set; 
+    String jornada; 
+    boolean mosca; 
+
+    public String getJornada() {
+        return jornada;
+    }
+
+    public void setJornada(String jornada) {
+        this.jornada = jornada;
+    }
+
+    
+    public boolean isMosca() {
+        return mosca;
+    }
+
+    public void setMosca(boolean mosca) {
+        this.mosca = mosca;
+    }
+
+    
+    public int getEquipo() {
+        return equipo;
+    }
+
+    public void setEquipo(int equipo) {
+        this.equipo = equipo;
+    }
+
+    public String getVolante() {
+        return volante;
+    }
+
+    public void setVolante(String volante) {
+        this.volante = volante;
+    }
+
+    public String getReferi() {
+        return referi;
+    }
+
+    public void setReferi(String referi) {
+        this.referi = referi;
+    }
+
+    public int getSet() {
+        return set;
+    }
+
+    public void setSet(int set) {
+        this.set = set;
+    }
+    
+   public double jugadas(){
+        return set;
+    }
+
+   
+}
